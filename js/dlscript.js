bf8174e0d4e82efcd583afc91533516eecbc2fcc
blckclov3r <blckclov3r@gmail.com>
2020-09-16 18:33:26 -0700

    commit

--- js/dlscript.js
+++ js/dlscript.js
@@ -1,20 +1,45 @@
 // JavaScript source code
 
+$(document).ready(function () {
 
-function screenshot() {
+    var specialElementHandlers = {
+        "#editor": function (element, renderer) {
+            return true;
+        }
+    };
 
-    html2Canvas(document.querySelector('#mContainer'), { useCORS: true }).then((canvas) => {
-        let pageData = canvas.toDataURL('image/jpeg', 1.0);
-    }
+    $("#mButton").click(function () {
+       /* var doc = new jsPDF();
+        doc.fromHTML($("#target").html(), 0, 0, {
+            "width": 100,
+            "elementHandlers": specialElementHandlers
+        });
+        doc.save("save-file.pdf"); */
 
-    html2canvas(document.getElementById('mContainer')).then(function (canvas) {
-        document.body.appendChild(canvas);
-        var imgdata = canvas.toDataURL('image/png');
-        var doc = new jsPDF();
-        doc.addImage(imgdata, 'PNG', 10, 10);
-        doc.save("slip.pdf");
+      /*  html2canvas(document.getElementById("mContainer")).then(function (canvas) {
+            var link = document.createElement("a");
+            document.body.appendChild(link);
+            link.download = "uv-slip.jpg";
+            link.href = canvas.toDataURL();
+            link.target = '_blank';
+            link.click();
+        }); */
+
+        var container = document.getElementById("mContainer"); // full page 
+        html2canvas(container, { allowTaint: true }).then(function (canvas) {
+
+            var link = document.createElement("a");
+            document.body.appendChild(link);
+            link.download = "html_image.png";
+            link.href = canvas.toDataURL("image/png");
+            link.target = '_blank';
+            link.click();
+        });
     });
 
+   
+});
 
-}
+
+
 
